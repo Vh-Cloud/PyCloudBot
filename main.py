c00ec58b608f1d562b1cec5c19a87406be2246ab
Marcos Paulo <76484463+mr-soulfox@users.noreply.github.com>
2021-05-16 00:16:40 -0300

    Update commands and add new commands

--- main.py
+++ main.py
@@ -15,6 +15,11 @@ async def on_ready():
     print(f'{bot.user.name} running.')
     await bot.change_presence(activity=discord.Streaming(name="%info", url="https://www.twitch.tv/123"))
 
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send('Por favor, passe todos os requerimentos :rolling_eyes:.')
+
 # Bot commands
 @bot.command()
 async def ping(ctx):
@@ -29,17 +34,37 @@ async def say(ctx, msg):
 @bot.command()
 async def create(ctx, name):
     createChannel = channels.ChannelsFunction(ctx, name)
-    await createChannel.create()
+    createChannel.create()
 
 @bot.command()
 async def delete(ctx, name):
     delChannel = channels.ChannelsFunction(ctx, name)
-    await delChannel.delete(discord)
+    delChannel.delete(discord)
 
 @bot.command()
 async def info(ctx):
     embedDiscord = embed.EmbedFunction(ctx)
-    await embedDiscord.embedCreate(discord)
+    embedDiscord.embedCreate(discord, 'info')
+
+@bot.command()
+async def help(ctx):
+    embedDiscord = embed.EmbedFunction(ctx)
+    await embedDiscord.embedCreate(discord, 'help')
+
+@bot.command()
+async def ban(ctx, member: discord.Member, reason=None):
+    if reason == None:
+        await ctx.send('Diga a razão do banimento')
     
+    else:
+        await member.ban(reason= reason)
+
+@bot.command()
+async def random(ctx, init=0, end=1):
+    value = int(random.randrange(init, end))
+    await ctx.send(value)
+    
+    
+
 #Run bot
 bot.run(token)
--- modules/embed.py
+++ modules/embed.py
@@ -4,27 +4,45 @@ class EmbedFunction:
     def __init__(self, ctx):
         self.context = ctx
     
-    async def embedCreate(self, discord):
+    async def embedCreate(self, discord, type):
         color = int(random.randrange(1, 2) - 1)
         colors = [0x005EFF, 0xFFFF00]
-        info = """
-            **💻 Infornações detalhadas**
-
-            **👨‍💻Desenvolvedor:**  `Marcos Paulo (mr-soulfox)`
-
-            **🏢Empresa:** `VH'Cloud`
-
-            **🤖 Meu prefixo:** `%` 
-
-            **💻 Host:** `Heroku`
-
-            **🐍Linguagem:** `Python`
-
-            **📖Biblioteca:** `Discord.py`
-
-            **Ver todos os comandos:** `%help`
-        """
-        embed = discord.message.Embed(color=colors[color], title="PyCloud info", description=info)
-        await self.context.message.channel.send(embed=embed)
-
         
+        if type == 'info':
+            info = """
+                **💻 Infornações detalhadas**
+
+                **👨‍💻Desenvolvedor:**  `Marcos Paulo (mr-soulfox)`
+
+                **🏢Empresa:** `Vh'Cloud`
+
+                **🤖 Meu prefixo:** `%` 
+
+                **💻 Host:** `Heroku`
+
+                **🐍Linguagem:** `Python`
+
+                **📖Biblioteca:** `Discord.py`
+
+                **Ver todos os comandos:** `%help`
+            """
+            embed = discord.message.Embed(color=colors[color], title="PyCloud info", description=info)
+            await self.context.message.channel.send(embed=embed)
+        
+        elif type == 'help':
+            commands = """
+                **Prefixo(%)**
+                
+                Comandos:
+                    **%ping - Retorna a latencia do PyCloud**
+                    **%say [palavra] - O PyCloud digita essa palavra**
+                    **%create [nome] - Cria um canal**
+                    **%delete [nome] - Deleta um canal**
+                    **%info - Traz informações sobre o PyCloud**
+                    **%help - Traz os comandos do PyCloud**
+                """
+            embed = discord.message.Embed(color=0xFFFF00, title="PyCloud Commands", description=commands)
+            await self.context.message.channel.send(embed=embed)
+        
+        else:
+            print('Nenhuma opção de ajuda escolhido')
